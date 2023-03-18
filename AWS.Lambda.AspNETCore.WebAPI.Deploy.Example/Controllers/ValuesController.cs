6959d6871ff4c47b9e6e38f779c0e91a1a54bf55
Gençay Yıldız <gyildizmail@gmail.com>
2023-03-18 22:49:06 +0300

    Add project files.

---
+++ AWS.Lambda.AspNETCore.WebAPI.Deploy.Example/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWS.Lambda.AspNETCore.WebAPI.Deploy.Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(DateTime.UtcNow);
+        }
+    }
+}
